7a2188f3bc52fc55a636d2e42d29d1b02309cec2
Juan Manuel <a01639125@tec.mx>
2025-11-08 22:41:38 -0600

    Chore: modify eslint.config to have jest-plugin for IDE support

--- Mas_Bosque_App/eslint.config.js
+++ Mas_Bosque_App/eslint.config.js
@@ -1,10 +1,15 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const jestPlugin = require('eslint-plugin-jest');
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+    expoConfig,
+    {
+        // apply the jest plugin configuration *only* to test files
+        files: ["**/*.test.js", "**/*.test.jsx", "**/*.test.ts", "**/*.test.tsx"],
+        ...jestPlugin.configs['flat/recommended'],   // use Jest plugin recommended rules for test files
+    },
+    {
+        ignores: ['dist/*'],
+    },
 ]);
